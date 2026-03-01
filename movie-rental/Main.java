82d40211339340e52c699dda867425318c9b7b5b
CassidyNP <cassidy12605@gmail.com>
2026-03-01 15:37:34 -0600

    added a second customer

--- movie-rental/Main.java
+++ movie-rental/Main.java
@@ -20,5 +20,23 @@ public class Main {
         System.out.println("\nXML Statement:\n");
         System.out.println(customer.xmlStatement());
 
+        /*-----Second Customer-----*/
+
+        Customer customer2 = new Customer("Elsha");
+
+        Movie regular2 = new RegularMovie("Hamnet");
+        Movie newRelease2 = new NewRelease("Bugonia");
+        Movie children2 = new ChildrenMovie("Moana");
+
+        customer2.addRental(new Rental(regular2, 7));
+        customer2.addRental(new Rental(newRelease2, 6));
+        customer2.addRental(new Rental(children2, 2));
+
+        System.out.println("\n");
+        System.out.println(customer2.outputStatement());
+        System.out.println("\nXML Statement:\n");
+        System.out.println(customer2.xmlStatement());
+
+
     }
 }
